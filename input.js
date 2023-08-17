72dc763166e6645c2711388df835afcb20589fcb
mattewkl <cikle290@gmail.com>
2023-08-17 13:51:05 +1000

    a lot

--- input.js
+++ input.js
@@ -1,7 +1,12 @@
 var COOKIE = document.getElementById("cookie-clicker");
 var SMALL_COOKIE_CLASS = 'cookie_small';
+var SHOP = document.querySelector('.shop');
+var OPEN_SHOP_BUTTON = document.querySelector('.header__button');
 var score = 0;
 var scoreElement = document.querySelector('.score');
+function toggleShop() {
+    SHOP.classList.toggle('shop_open');
+}
 function cookieClickHandler() {
     console.log('click');
 }
@@ -24,3 +29,4 @@ function toggleCookieClasses() {
 // COOKIE.addEventListener('click',cookieClickHandler)
 COOKIE.addEventListener('mousedown', cookieMouseDownHandler);
 COOKIE.addEventListener('mouseup', cookieMouseUpHandler);
+OPEN_SHOP_BUTTON.addEventListener('click', toggleShop);
--- input.ts
+++ input.ts
@@ -1,7 +1,16 @@
 const COOKIE: HTMLElement = document.getElementById("cookie-clicker")
 const SMALL_COOKIE_CLASS: string = 'cookie_small'
+const SHOP:HTMLElement = document.querySelector('.shop')
+const OPEN_SHOP_BUTTON:HTMLElement = document.querySelector('.header__button')
 let score:number = 0
 let scoreElement:HTMLElement = document.querySelector('.score')
+
+
+function toggleShop():void {
+    SHOP.classList.toggle('shop_open')
+}
+
+
 
 function cookieClickHandler(): void {
     console.log('click')
@@ -31,3 +40,5 @@ function toggleCookieClasses(): void {
 // COOKIE.addEventListener('click',cookieClickHandler)
 COOKIE.addEventListener('mousedown', cookieMouseDownHandler)
 COOKIE.addEventListener('mouseup', cookieMouseUpHandler)
+
+OPEN_SHOP_BUTTON.addEventListener('click', toggleShop)
